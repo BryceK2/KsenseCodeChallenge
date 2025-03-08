30e471eda0a15fec08a65f861743c26f2be13d7b
BryceK2 <brke4424@colorado.edu>
2025-03-07 19:45:22 -0700

    Updated routes to Next 13 syntax

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,27 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { kv } from "@vercel/kv";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
-    console.log("Received Payload:", req.body);
+// POST route to handle payload
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  console.log("Received Payload:", req.body);
 
-    // store secret message in Vercel's KV db
-    await kv.set("secret_message", req.body);
-    return res.status(200).json({ msg: "Your secret is safe with me" });
+  // store secret message in Vercel's KV db
+  await kv.set("secret_message", req.body);
+  return res.status(200).json({ msg: "Your secret is safe with me" });
+}
+
+// GET route for retrieval once payload sent
+// I can check Vercel's KV db for speed, but this route is for you to check for storage
+export async function GET(res: NextApiResponse) {
+  const payload = await kv.get("secret_message");
+  if (!payload) {
+    return res.status(400).json({ error: "No secret message stored." });
   }
-
-  // get route for retrieval once payload sent
-  // I can check Vercel's KV db for speed, but this route is for you to check for storage
-  if (req.method === "GET") {
-    const payload = await kv.get("secret_message");
-    if (!payload) {
-      return res.status(400).json({ error: "No secret message stored." });
-    }
-    return res.status(200).json(payload);
-  }
-
-  return res.status(405).json({ error: "Only POST/GET methods are allowed" });
+  return res.status(200).json(payload);
 }
