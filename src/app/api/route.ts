fcaaeeace9207c0935221c41d39591f5dca06174
BryceK2 <brke4424@colorado.edu>
2025-03-07 19:54:39 -0700

    Updated api to latest next.js api format

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,21 +1,22 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 import { kv } from "@vercel/kv";
 
 // POST route to handle payload
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  console.log("Received Payload:", req.body);
+export async function POST(request: Request) {
+  const body = await request.json();
+  console.log("Received Payload:", body);
 
   // store secret message in Vercel's KV db
-  await kv.set("secret_message", req.body);
-  return res.status(200).json({ msg: "Your secret is safe with me" });
+  await kv.set("secret_message", body);
+  return NextResponse.json({ msg: "Your secret is safe with me" });
 }
 
 // GET route for retrieval once payload sent
 // I can check Vercel's KV db for speed, but this route is for you to check for storage
-export async function GET(res: NextApiResponse) {
+export async function GET(request: Request) {
   const payload = await kv.get("secret_message");
   if (!payload) {
-    return res.status(400).json({ error: "No secret message stored." });
+    return NextResponse.json({ error: "No secret message stored." });
   }
-  return res.status(200).json(payload);
+  return NextResponse.json(payload);
 }
